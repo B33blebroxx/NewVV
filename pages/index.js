96c51f895cce3039af7076b2e70bad81d1abf274
B33blebroxx <b33blebroxx@gmail.com>
2024-08-06 14:40:54 -0500

    Added logo and mission statement with minimal styling to index

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
+import Image from 'next/image';
+import logo from '../utils/data/ValVenisLogo.png';
 
 function Home() {
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello! </h1>
-
+    <div className="container">
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Image src={logo} alt="Logo" width={300} height={250} style={{ marginBottom: '20px' }} />
+      </div>
+      <br />
+      <div>
+        <Card>
+          <h1>Welcome to ValVenis.com!</h1>
+          <br />
+          <h5>Our mission is to create a safe, inclusive, and supportive online space for LGBTQIA+ individuals. We aim to connect people with essential resources, information, and communities that uplift and empower them. Our goal is to ensure that every person can access the support they need to thrive, celebrate their identity, and build a fulfilling life.</h5>
+        </Card>
+      </div>
     </div>
   );
 }
