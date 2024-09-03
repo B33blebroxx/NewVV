cff087124476dadec1cf13d4e4b6a4b4a783e15c
B33blebroxx <b33blebroxx@gmail.com>
2024-09-03 09:28:50 -0500

    Made quotes page elements reliant on dynamic backend data instead of hardcoded in

---
+++ api/quotePageApi.js
@@ -0,0 +1,26 @@
+const databaseUrl = 'https://localhost:7067';
+
+const getQuotePageInfo = async () => {
+  const response = await fetch(`${databaseUrl}/quotePage`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const data = await response.json();
+  return data;
+};
+
+const editQuotePageInfo = async (quotePage) => {
+  const response = await fetch(`${databaseUrl}/quotePage`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(quotePage),
+  });
+  const data = await response.json();
+  return data;
+};
+
+export { getQuotePageInfo, editQuotePageInfo };
